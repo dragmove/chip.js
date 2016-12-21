d8ee578fbf9ea149ff8690751a0c41c1b69a9be2
김현석 <vuild@ncsoft.com>
2016-12-21 18:03:00 +0900

    HorizontalScrollNavi working

--- app/src/component/HorizontalScrollNavi.js
+++ app/src/component/HorizontalScrollNavi.js
@@ -2,49 +2,178 @@ import Navi from './Navi';
 
 class HorizontalScrollNavi extends Navi {
   constructor(options) {
-    if(!window.Dragdealer) {
+    if (!window.Dragdealer) {
       throw new Error('HorizontalScrollNavi.js require Dragdealer.js Library.');
     }
+    if (!options) return;
 
-    if(!options) return;
-    super(options);
+    let opt = {
+      /*
+      // Navi.js options
+       btns,
+       mouseoverCallback,
+       mouseoutCallback,
+       clickCallback,
+       activateCallback,
+       */
 
+      wrap: null,
+      handleClass: '',
+      btnsWrap: null,
+      disabled: false,
+      slide: true,
+      loose: true,
+      speed: 0.25,
+      css3: true,
+      scrollCallback: null
+    };
+    $.extend(opt, options);
+
+    super(opt);
+
+    let _ = this;
+
+    _.option = opt;
+
+    _.isNeedScroll = false;
+
+    _.dragDealer = null;
+
+    _.$proxyResize = $.proxy(_.resize, _);
+  }
+
+  init(obj) {
+    super.init(obj);
+
+    this.setInstance();
+  }
+
+  setInstance() {
+    let _ = this;
+
+    let opt = _.option;
+    console.log('opt :', opt);
+
+    _.dragDealer = new window.Dragdealer($(opt.wrap).get(0), {
+      handleClass: opt.handleClass,
+      disabled: opt.disabled,
+      horizontal: true,
+      vertical: false,
+      slide: opt.slide,
+      loose: opt.loose,
+      speed: opt.speed,
+      css3: opt.css3,
+      callback: opt.scrollCallback
+    });
+
+    /*
     // TODO
+    $(window).on('resize orientationchange', _.$proxyResize);
+    _.resize();
+    */
+  }
+
+  resize(evt) {
+    // TODO
+    console.log('resize');
+
+    let _ = this;
+
+    if(!_.dragDealer) return;
+
+    if (_.option.btnsWrap.width() > window.innerWidth) {
+      _.dragDealer.enable();
+
+      if(!isMobile.any) {
+        _.dragDealer.disable();
+      }
+
+      _.isNeedScroll = true;
+
+    } else {
+      _.dragDealer.disable();
+
+      _.isNeedScroll = false;
+
+      _.setDragDealerPosition(0);
+    }
   }
 
   /*
-  constructor(options) {
-    if(!options) return;
-    super(options);
+   * public methods
+   */
+  // getBtns()
+  // getBtn(index)
+  // getActivatedIndex()
+  // activate(index)
 
-    this.timerInterval = (options.timerInterval >= 0 || typeof options.timerInterval !== 'undefined') ? options.timerInterval : 500;
-    this.timer = null;
+  getRatiosByOffsets(x) {
+    // TODO
+    return this.dragDealer.getRatiosByOffsets([x, 0]);
   }
 
-  mouseoverBtnEventHandler(evt) {
-    this.removeTimer();
-    super.mouseoverBtnEventHandler(evt);
+  setDragDealerPosition(x) {
+    // TODO
+    let offset = this.dragDealer.getRatiosByOffsets([x, 0]);
+    this.dragDealer.setValue(offset[0], offset[1]);
   }
 
-  mouseoutBtnEventHandler(evt) {
-    let _ = this;
-
-    super.mouseoutBtnEventHandler(evt);
-
-    _.timer = setTimeout(() => {
-      _.activateByTimer(_.activateIndex);
-    }, _.timerInterval);
+  setValue(x, y) {
+    // TODO
+    this.dragDealer.setValue(x, y);
   }
 
-  activateByTimer(index) {
-    super.activate(index);
+  resize() {
+    // TODO
   }
 
-  removeTimer() {
-    if(this.timer) clearTimeout(this.timer);
-    this.timer = null;
+  enable() {
+    this.dragDealer.enable();
   }
-  */
+
+  disable() {
+    this.dragDealer.disable();
+  }
+
+  destroy(obj) {
+    // TODO
+
+    super.destroy(obj);
+  }
+
+  /*
+   constructor(options) {
+   if(!options) return;
+   super(options);
+
+   this.timerInterval = (options.timerInterval >= 0 || typeof options.timerInterval !== 'undefined') ? options.timerInterval : 500;
+   this.timer = null;
+   }
+
+   mouseoverBtnEventHandler(evt) {
+   this.removeTimer();
+   super.mouseoverBtnEventHandler(evt);
+   }
+
+   mouseoutBtnEventHandler(evt) {
+   let _ = this;
+
+   super.mouseoutBtnEventHandler(evt);
+
+   _.timer = setTimeout(() => {
+   _.activateByTimer(_.activateIndex);
+   }, _.timerInterval);
+   }
+
+   activateByTimer(index) {
+   super.activate(index);
+   }
+
+   removeTimer() {
+   if(this.timer) clearTimeout(this.timer);
+   this.timer = null;
+   }
+   */
 }
 
 export default HorizontalScrollNavi;
--- app/src/main.js
+++ app/src/main.js
@@ -7,7 +7,7 @@
 // import FullSizeCanvasVideo from './component/FullSizeCanvasVideo';
 import HorizontalScrollNavi from './component/HorizontalScrollNavi';
 
-(function($) {
+(function ($) {
   "use strict";
   $(document).ready(init);
 
@@ -23,9 +23,139 @@ import HorizontalScrollNavi from './component/HorizontalScrollNavi';
   }
 
   function testHorizontalScrollNavi() {
+    let btnsWrap = $('.slidetab__tab');
+
     let scrollNavi = new HorizontalScrollNavi({
-      // TODO
+      // Navi.js options
+      btns: $('li a', btnsWrap),
+      mouseoverCallback: mouseoverCallback,
+      mouseoutCallback: mouseoutCallback,
+      clickCallback: clickCallback,
+      activateCallback: activateCallback,
+
+      // HorizontalScrollNavi.js options
+      wrap: $('.slidetab'),
+      handleClass: 'slidetab__handle',
+      btnsWrap: btnsWrap,
+
+      disabled: false,
+      slide: true,
+      loose: true,
+      speed: 0.25,
+      css3: true,
+      scrollCallback: scrollCallback
     });
     scrollNavi.init();
+
+    function mouseoverCallback(obj) {
+      // console.log('mouseover :', obj);
+    }
+
+    function mouseoutCallback(obj) {
+      // console.log('mouseout :', obj);
+    }
+
+    function clickCallback(obj) {
+      console.log('click :', obj);
+    }
+
+    function activateCallback(obj) {
+      console.log('activateCallback :', obj);
+
+      let btns = $(scrollNavi.getBtns()),
+        btn = $(scrollNavi.getBtn(obj.index));
+
+      btns.removeClass('on');
+      btn.addClass('on');
+    }
+
+    function scrollCallback(x, y) {
+      console.log('scroll x, y :', x, y);
+
+    }
+
+    // TEST
+    /*
+     $('.test-btns a').on('click', function (evt) {
+     let index = $(this).index() + 1;
+     activateScrollNaviByExternal(index);
+     });
+
+     function activateScrollNaviByExternal(index) {
+     if (scrollNavi) scrollNavi.activate(index);
+
+     // move scroll navi
+     if (index < 1 || index > scrollNavi.getBtns().length) return;
+
+     let prev = (index <= 1) ? 0 : index - 1,
+     next = (index > scrollNavi.getBtns().length) ? 0 : index + 1;
+
+     if (!prev) {
+     // go to left end.
+     setDragDealerPosition(0, 0);
+     return;
+     }
+
+     if (!next) {
+     // go go right end.
+     scrollNavi.setValue(1, 0);
+     return;
+     }
+
+     let btn = $(scrollNavi.getBtn(prev));
+     if (btn.length) setDragDealerPosition(-btn.position().left);
+     }
+     */
+
+    /*
+     function createCanvasMask() {
+     if (!scrollNavi) {
+     scrollNavi = new HorizontalScrollNavi({
+     btns: $('.slidetab__tab li a'),
+     mouseoverCallback: mouseoverCallback,
+     mouseoutCallback: mouseoutCallback,
+     clickCallback: clickCallback,
+     activateCallback: activateCallback
+     });
+     scrollNavi.init();
+     } else {
+     scrollNavi.enable();
+     }
+
+     if (!isMobile.any) {
+     // desktop - disable drag
+     scrollNavi.disable();
+     }
+
+     isNeedScroll = true;
+     }
+
+     $(window).on('resize orientationchange', resize);
+     resize();
+
+     function resize(evt) {
+     console.log('resize');
+
+     if (btnWrap.width() > window.innerWidth) {
+     createCanvasMask();
+     } else {
+     if (scrollNavi) {
+     scrollNavi.disable();
+
+     isNeedScroll = false;
+
+     setDragDealerPosition(0);
+     }
+     }
+     }
+
+     function setDragDealerPosition(x) {
+     // var offset = this.dragDealer.getRatiosByOffsets([x, 0]);
+     let offset = scrollNavi.getRatiosByOffsets(x);
+     scrollNavi.setValue(offset[0], offset[1]);
+     }
+
+     */
   }
+
 }(jQuery));
