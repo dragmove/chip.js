55479095375e381a3acc9b8b7f9897bbb88e683b
김현석 <dragmove@naver.com>
2017-04-26 11:14:06 +0900

    update Overlay

--- app/src/component/Overlay.js
+++ app/src/component/Overlay.js
@@ -11,6 +11,27 @@
  }).init();
 
  overlay.show();
+
+ // set node event handler
+ // overlay.setNodeEventHandler(true/false);
+
+ // get node
+ // console.log(overlay.getNode());
+
+ // set css
+ // overlay.setCss({'background-color': '#f00', ...});
+
+ // append to other element
+ // overlay.appendTo(parent element);
+
+ // show overlay
+ // overlay.show();
+
+ // hide overlay
+ // overlay.hide();
+
+ // destroy overlay
+ // overlay.destroy();
  */
 
 class Overlay {
--- app/src/main.js
+++ app/src/main.js
@@ -40,6 +40,30 @@ import Dropdown from './component/Dropdown';
       }
     }).init();
 
+    /*
+     * overlay public methods
+     */
+    // set node event handler
+    // overlay.setNodeEventHandler(true/false);
+
+    // get node
+    // console.log(overlay.getNode());
+
+    // set css
+    // overlay.setCss({'background-color': '#f00', ...});
+
+    // append to other element
+    // overlay.appendTo(parent element);
+
+    // show overlay
+    // overlay.show();
+
+    // hide overlay
+    // overlay.hide();
+
+    // destroy overlay
+    // overlay.destroy();
+
     return overlay;
   }
 
